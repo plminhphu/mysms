b0c47609739b45e0a626b9cbc42194dafcd1cea0
plminhphu <phamleminhphu123@gmail.com>
2024-04-27 20:53:44 +0700

    chage code

--- smsapi/lib/main.dart
+++ smsapi/lib/main.dart
@@ -1,53 +1,64 @@
+// import 'package:background_sms/background_sms.dart';
 import 'package:flutter/material.dart';
+// import 'package:workmanager/workmanager.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Workmanager().initialize(callbackDispatcher, isInDebugMode: true);
+  // Workmanager().registerPeriodicTask(
+  //   "task-identifier",
+  //   "simpleTask",
+  //   frequency: const Duration(seconds: 1),
+  // );
+  // AwesomeNotifications().initialize(
+  //     // set the icon to null if you want to use the default app icon
+  //     'resource://drawable/res_app_icon',
+  //     [
+  //       NotificationChannel(
+  //           channelGroupKey: 'basic_channel_group',
+  //           channelKey: 'basic_channel',
+  //           channelName: 'Basic notifications',
+  //           channelDescription: 'Notification channel for basic tests',
+  //           defaultColor: const Color.fromARGB(255, 36, 30, 216),
+  //           ledColor: Colors.white)
+  //     ],
+  //     channelGroups: [
+  //       NotificationChannelGroup(
+  //           channelGroupKey: 'basic_channel_group',
+  //           channelGroupName: 'Basic group')
+  //     ],
+  //     debug: true);
   runApp(const MyApp());
 }
 
+// @pragma('vm:entry-point')
+// void callbackDispatcher() {
+//   Workmanager().executeTask((task, inputData) {
+//     print("Native called background task: $task");
+//     return Future.value(true);
+//   });
+// }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'PL Minh Phú SMS',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        useMaterial3: false,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(title: 'PL Minh Phú SMS'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  static final GlobalKey<NavigatorState> navigatorKey =
+      GlobalKey<NavigatorState>();
+
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -55,71 +66,62 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  Future _sendSMS(String message, List<String> recipents) async {
+    // AwesomeNotifications().createNotification(
+    //     content: NotificationContent(
+    //   id: 10,
+    //   channelKey: 'basic_channel',
+    //   actionType: ActionType.Default,
+    //   title: 'Hello World!',
+    //   body: 'This is my first notification!',
+    //   wakeUpScreen: true,
+    //   badge: 1,
+    // ));
+    // for (var recipent in recipents) {
+    //   SmsStatus result = await BackgroundSms.sendMessage(
+    //       phoneNumber: recipent, message: message);
+    //   if (result == SmsStatus.sent) {
+    //     print("Sent");
+    //   } else {
+    //     print("Failed");
+    //   }
+    // }
+  }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    // AwesomeNotifications().isNotificationAllowed().then((isAllowed) {
+    //   if (!isAllowed) {
+    //     AwesomeNotifications().requestPermissionToSendNotifications();
+    //   }
+    // });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: const Center(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'You have pushed the button this many times:',
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () async {
+          String message = "This is a test message!";
+          List<String> recipents = ["0329886884"];
+          await _sendSMS(message, recipents);
+        },
+        child: const Icon(Icons.question_mark_outlined),
+      ),
     );
   }
 }
